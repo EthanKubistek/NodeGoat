bdcf8d581495746be27d97cfb2e04a8a0b1ee97b
Ethan Kubistek <ekubistek@gmail.com>
2020-03-12 12:22:10 -0400

    A2-2

--- app/routes/session.js
+++ app/routes/session.js
@@ -58,8 +58,7 @@ function SessionHandler(db) {
         userDAO.validateLogin(userName, password, function(err, user) {
             console.log(user)
             var errorMessage = "Invalid username and/or password";
-            var invalidUserNameErrorMessage = "Invalid username";
-            var invalidPasswordErrorMessage = "Invalid password";
+
             if (err) {
                 if (err.noSuchUser) {
 
@@ -80,18 +79,18 @@ function SessionHandler(db) {
                     return res.render("login", {
                         userName: userName,
                         password: "",
-                        loginError: invalidUserNameErrorMessage
+
                         //Fix for A2-2 Broken Auth - Uses identical error for both username, password error
-                        // loginError: errorMessage
+                        loginError: errorMessage
                     });
                 } else if (err.invalidPassword) {
                     return res.render("login", {
                         userName: userName,
                         password: "",
-                        loginError: invalidPasswordErrorMessage
-                        //Fix for A2-2 Broken Auth - Uses identical error for both username, password error
-                        // loginError: errorMessage
 
+                        //Fix for A2-2 Broken Auth - Uses identical error for both username, password error
+
+                         loginError: errorMessage
                     });
                 } else {
                     return next(err);
@@ -144,12 +143,12 @@ function SessionHandler(db) {
         var FNAME_RE = /^.{1,100}$/;
         var LNAME_RE = /^.{1,100}$/;
         var EMAIL_RE = /^[\S]+@[\S]+\.[\S]+$/;
-        var PASS_RE = /^.{1,20}$/;
-        /*
+
+
         //Fix for A2-2 - Broken Authentication -  requires stronger password
         //(at least 8 characters with numbers and both lowercase and uppercase letters.)
         var PASS_RE =/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
-        */
+        
 
         errors.userNameError = "";
         errors.firstNameError = "";
